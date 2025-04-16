58c03f5df3ffb8e8f9ee86abdfae98b92d391e09
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-16 13:25:42 +0530

    changes

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ resource "aws_eks_cluster" "main" {
   version  = var.eks_version
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = true
   }
 
   depends_on = [
