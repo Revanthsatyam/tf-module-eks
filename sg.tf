9645efd0849a834378b208da5e437f7770c4b54d
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-17 17:44:03 +0530

    changes

--- sg.tf
+++ sg.tf
@@ -20,11 +20,11 @@ resource "aws_security_group" "cluster" {
   }
 }
 
-# resource "aws_security_group_rule" "main" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = var.sg_ingress_cidr
-#   security_group_id = aws_eks_cluster.main.
-# }
+resource "aws_security_group_rule" "main" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = data.aws_eks_cluster.cluster.vpc_config[0]["cluster_security_group_id"]
+}
