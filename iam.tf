042ba8bf93f432f46f78f3f0ba590cc3d63b7fe1
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-20 21:25:20 +0530

    changes

--- iam.tf
+++ iam.tf
@@ -57,3 +57,46 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.worker.name
 }
+
+resource "aws_iam_role" "main" {
+  name               = "${local.name_prefix}-parameters-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, { Name = "${local.name_prefix}-role" })
+}
+
+resource "aws_iam_policy" "main" {
+  name        = "${local.name_prefix}-parameters-policy"
+  description = "${local.name_prefix}-parameters-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Decrypt",
+          #"kms:GenerateDataKey*",
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : ["arn:aws:ssm:us-east-1:058264090525:parameter/*", var.kms_key_arn]
+      }
+    ]
+  })
+}
--- vars.tf
+++ vars.tf
@@ -5,3 +5,4 @@ variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "sg_port" {}
 variable "sg_ingress_cidr" {}
+variable "kms_key_arn" {}
