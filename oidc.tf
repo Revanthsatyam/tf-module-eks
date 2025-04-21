b19ef95196a0d8a24ddeb79f7656e522e90bf4b1
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 11:21:48 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -1,10 +1,14 @@
-data "external" "thumb" {
-  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity.0.oidc.0["issuer"]]
+data "http" "oidc_metadata" {
+  url = "${aws_eks_cluster.main.identity[0].oidc[0]["issuer"]}/.well-known/openid-configuration"
+}
+
+data "tls_certificate" "oidc_cert" {
+  url = jsondecode(data.http.oidc_metadata.body)["issuer"] # URL for certificate
 }
 
 resource "aws_iam_openid_connect_provider" "main" {
   depends_on      = [aws_eks_cluster.main]
   url             = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.external.thumb.result.thumbprint]
+  thumbprint_list = [data.tls_certificate.oidc_cert.certificates[0].thumbprint]
 }
