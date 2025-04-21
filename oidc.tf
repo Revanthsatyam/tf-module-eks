19448c5eed225c940d62e80b401a74cd505ed2c0
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 11:24:46 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -2,13 +2,25 @@ data "http" "oidc_metadata" {
   url = "${aws_eks_cluster.main.identity[0].oidc[0]["issuer"]}/.well-known/openid-configuration"
 }
 
+# Fetch the certificate PEM using tls_certificate
 data "tls_certificate" "oidc_cert" {
-  url = jsondecode(data.http.oidc_metadata.body)["issuer"] # URL for certificate
+  url = jsondecode(data.http.oidc_metadata.response_body)["issuer"]
+}
+
+# Generate the thumbprint from the certificate
+locals {
+  thumbprint = chomp(
+    base64encode(
+      sha1(
+        data.tls_certificate.oidc_cert.certificates
+      )
+    )
+  )
 }
 
 resource "aws_iam_openid_connect_provider" "main" {
   depends_on      = [aws_eks_cluster.main]
   url             = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_cert.certificates[0].thumbprint]
+  thumbprint_list = [local.thumbprint]
 }
