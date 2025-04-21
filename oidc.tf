27e53b8611dab944c9f9d16375cf050236a5fd65
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 11:13:57 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -3,8 +3,8 @@ data "external" "thumb" {
 }
 
 resource "aws_iam_openid_connect_provider" "main" {
-  depends_on = [aws_eks_cluster.main]
-  url        = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
-  client_id_list = ["sts.amazonaws.com"]
+  depends_on      = [aws_eks_cluster.main]
+  url             = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.external.thumb.result.thumbprint]
 }
