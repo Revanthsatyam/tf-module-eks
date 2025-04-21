14b09b4cde4e946a4bc1a1dfd3382efa91de27ac
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 10:58:40 +0530

    changes

--- data.tf
+++ data.tf
@@ -1,3 +1,7 @@
 data "aws_eks_cluster" "cluster" {
   name = aws_eks_cluster.main.name
 }
+
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity.0.oidc.0["issuer"]]
+}
