cc02ca42fdbed4a8b00cf874e15556e9404522f7
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 10:38:50 +0530

    changes

---
+++ oidc.tf
@@ -0,0 +1,10 @@
+# resource "aws_eks_identity_provider_config" "main" {
+#   depends_on   = [aws_eks_cluster.main]
+#   cluster_name = aws_eks_cluster.main.name
+#
+#   oidc {
+#     client_id                     = "sts.amazonaws.com"
+#     identity_provider_config_name = "${local.name_prefix}-cluster-oidc"
+#     issuer_url                    = ""
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "eks_cluster" {
-  value = data.aws_eks_cluster.cluster.vpc_config[0]["cluster_security_group_id"]
+  value = aws_eks_cluster.main
 }
