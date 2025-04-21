f9cc3a2bad9afcf048a42e994a53fe8c739acb44
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 11:13:10 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -1,9 +1,10 @@
-# resource "aws_iam_openid_connect_provider" "main" {
-#   depends_on = [aws_eks_cluster.main]
-#   url        = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
-#   client_id_list = [
-#     "sts.amazonaws.com",
-#   ]
-#
-#   thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
-# }
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity.0.oidc.0["issuer"]]
+}
+
+resource "aws_iam_openid_connect_provider" "main" {
+  depends_on = [aws_eks_cluster.main]
+  url        = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.external.thumb.result.thumbprint]
+}
