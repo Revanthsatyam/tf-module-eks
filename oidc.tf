20b0c5ab7040e1acc4cd9021685d3c2203f3648b
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 10:57:15 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -1,10 +1,9 @@
-resource "aws_eks_identity_provider_config" "main" {
-  depends_on   = [aws_eks_cluster.main]
-  cluster_name = aws_eks_cluster.main.name
-
-  oidc {
-    client_id                     = "sts.amazonaws.com"
-    identity_provider_config_name = "${local.name_prefix}-cluster-oidc"
-    issuer_url                    = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
-  }
-}
+# resource "aws_iam_openid_connect_provider" "main" {
+#   depends_on = [aws_eks_cluster.main]
+#   url        = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
+#   client_id_list = [
+#     "sts.amazonaws.com",
+#   ]
+#
+#   thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+# }
