b91fcf7d2572ef7129a5de962746745a79c6ce6d
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 11:36:09 +0530

    changes

--- oidc.tf
+++ oidc.tf
@@ -7,20 +7,13 @@ data "tls_certificate" "oidc_cert" {
   url = jsondecode(data.http.oidc_metadata.response_body)["issuer"]
 }
 
-# Generate the thumbprint from the first certificate in the list
-# locals {
-#   thumbprint = chomp(
-#     base64encode(
-#       sha1(
-#         data.tls_certificate.oidc_cert.certificate
-#       )
-#     )
-#   )
-# }
-#
-# resource "aws_iam_openid_connect_provider" "main" {
-#   depends_on      = [aws_eks_cluster.main]
-#   url             = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = [local.thumbprint]
-# }
+locals {
+  thumbprint = element(data.tls_certificate.oidc_cert.certificates, length(data.tls_certificate.oidc_cert.certificates) - 1).sha1_fingerprint
+}
+
+resource "aws_iam_openid_connect_provider" "main" {
+  depends_on      = [aws_eks_cluster.main]
+  url             = aws_eks_cluster.main.identity[0].oidc[0]["issuer"]
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [local.thumbprint]
+}
